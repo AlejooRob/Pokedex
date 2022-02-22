0711f82edec321045c8ad1769c04d09c64846a61
Alejo <alejolink001@gmail.com>
2022-02-22 11:53:37 -0500

    Change stack Navigator by Tabs Navigator

---
+++ src/screens/SearchScreen.tsx
@@ -0,0 +1,60 @@
+import { View, Platform, Text, FlatList, Dimensions } from 'react-native'
+import React from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { SearchInput } from '../components/SearchInput';
+import { usePokemonSearch } from '../hooks/usePokemonSearch';
+import { styles  } from "../theme/appTheme";
+import PokemonCard from '../components/PokemonCard';
+import Loading from '../components/Loading';
+
+const screenWidth = Dimensions.get('window').width
+
+const SearchScreen = () => {
+
+    const { top } = useSafeAreaInsets();
+    const { isFetching, simplePokemonList } = usePokemonSearch();
+
+    if( isFetching ) {
+        return <Loading />
+    }
+
+  return (
+    <View style={{
+        flex:1,
+        marginHorizontal: 20
+    }}>
+      
+        <SearchInput 
+            style={{
+                position: 'absolute',
+                zIndex: 999,
+                width: screenWidth - 40,
+                top: ( Platform.OS === 'ios') ? top : top + 30 
+            }}
+        />
+
+        <FlatList 
+            data={ simplePokemonList }
+            keyExtractor= { (pokemon) => pokemon.id }
+            showsVerticalScrollIndicator={ false }
+            numColumns={ 2 }
+
+            // header
+            ListHeaderComponent={(
+                <Text style={{
+                ...styles.title,
+                ...styles.globalMargin,
+                paddingBottom: 10,
+                marginTop: ( Platform.OS === 'ios') ? top + 60 : top + 80
+                }}>Pokedex</Text>
+            )}
+            renderItem={ ({ item }) => <PokemonCard pokemon={ item } />}
+
+        />
+
+    </View>
+  )
+}
+
+
+export default SearchScreen
